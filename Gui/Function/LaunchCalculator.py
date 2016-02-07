3114cbbbb2617b2311fcd02724308406772965c2
tinmarino <tinmarino@gmail.com>
2016-02-07 20:45:32 +0100

    Still in menuBar

--- Gui/Function/LaunchCalculator.py
+++ Gui/Function/LaunchCalculator.py
@@ -1,29 +1,36 @@
-from Tkinter import *
+import Tkinter as Tk
 
 
 import Calculator as Cal
 import GuyVariables as G
 
+
 def Calculator():  # On LeftTopFrame
-    if G.tutorial:
-        text  = "This button create a Calculator in the G.LabelFrame. You can put the cursor in the text entry and type with the keyboard and press enter to get the result or clisk on the button and '=' returns the answer. Numpy is automatically import as *"
-        text += "\n\nProgrammers a memory would be user firendly, but anyway, nobody will use this calculator, bc or python or awk are much  better..."
-        MG.TutorialReturn({"title": "Calculator", "text": text})
-        return
+    """
+    This button create a Calculator in the G.LabelFrame.
+    You can put the cursor in the text entry and type with the keyboard and
+    press enter to get the result or clisk on the button and '=' returns the
+    answer. Numpy is automatically import as *"
+
+    Programmers a memory would be user firendly, but anyway,
+    nobody will use this calculator,
+    bc or python or awk are much  better..."
+    """
     G.cal_bool = not G.cal_bool
 
 
-
     # CONSTRUCT
-    if G.cal_bool :
+    if G.cal_bool:
 
         # FRAME
-        G.CalculatorFrame = Frame( G.LeftTopFrame , **G.fr_arg )
-        G.CalculatorFrame.pack(side=TOP, expand=0, fill=X)
+        G.CalculatorFrame = Tk.Frame(G.LeftTopFrame, **G.fr_arg)
+        G.CalculatorFrame.pack(side=Tk.TOP, expand=0, fill=Tk.X)
         # TITLE
-        Label(G.CalculatorFrame, text="Calculator", **G.frame_title_arg).pack(side=TOP, anchor="w")
+        Tk.Label(G.CalculatorFrame,
+                 text="Calculator",
+                 **G.frame_title_arg).pack(side=Tk.TOP, anchor="w")
         G.all_frame.append("CalculatorFrame")
-        Cal.MyCalculator( G.CalculatorFrame )
+        Cal.MyCalculator(G.CalculatorFrame)
 
 
     # DESTROY
@@ -33,10 +40,10 @@ def Calculator():  # On LeftTopFrame
 
 
     # Change tool menu label
-    for i in range(1,10) :
-        j = G.tool_menu.menu.entrycget(i,"label")
+    for i in range(1, 0) :
+        j = G.tool_menu.menu.entrycget(i, "label")
         if "Calculator" in j:
-            if G.cal_bool : G.tool_menu.menu.entryconfig(i,label=u'\u25b4 '+'Calculator')
-            else          : G.tool_menu.menu.entryconfig(i,label=u'\u25be '+'Calculator')
+            if G.cal_bool:  G.tool_menu.menu.entryconfig(i, label=u'\u25b4 '+'Calculator')
+            else:           G.tool_menu.menu.entryconfig(i, label=u'\u25be '+'Calculator')
             break
     return
--- Gui/Menu/MenuBar.py
+++ Gui/Menu/MenuBar.py
@@ -1,6 +1,7 @@
 from Tkinter import *
 
 import FileMenu
+import AbismMenu
 import GuyVariables as G
 import WorkVariables as W
 
@@ -19,189 +20,22 @@ def MenuBarMaker():             # CALLER
     args = G.me_arg.copy()
     args.update (  {"relief":FLAT, "width":G.menu_button_width }  )
     for i in [
-        ["AbismMenu", {"text":u"\u25be "+"ABISM"}           ],
-        ["1", {"text":u"\u25be "+"File"}  ] ,
-        ["AnalysisMenu",  {"text":u'\u25be '+'Analysis'} ] ,
-        ["ViewMenu",  {"text":u'\u25be '+'View'} ],
-        ["ToolMenu",  {"text":u'\u25be '+'Tools'} ],
+        [AbismMenu.AbismMenu, {"text":u"\u25be "+"ABISM"}           ],
+        [FileMenu.FileMenu, {"text":u"\u25be "+"File"}  ] ,
+        [AnalysisMenu,  {"text":u'\u25be '+'Analysis'} ] ,
+        [ViewMenu,  {"text":u'\u25be '+'View'} ],
+        [ToolMenu,  {"text":u'\u25be '+'Tools'} ],
     ] :
         args.update( i[1] )
-        if (i[0]=="1"):
-            button = FileMenu.FileMenu(args)
-        else :
-            button = globals()[i[0]]() # the actual CALL
+        button = i[0](args)
         G.MenuBar.columnconfigure(col,weight=1)
         button.grid(row=0,column=col,sticky="nsew")
 
         col+=1
 
 
-def AbismMenu():                # Menu entry to configure ABSIM
-    G.abism_menu=Menubutton(G.MenuBar,**args)
-    G.abism_menu.menu=Menu(G.abism_menu,**G.submenu_args)
 
-    # BACKGROUND FOREGROUND
-    def AppearanceMenu(parent_menu,who="bg",label="Background") :
-      def CrazyColor(who="bg") :
-         import color_interactive
-         tmp = ( lambda x, who=who : BgCl(color=x,who=who )   )
-         color_interactive.Main( func = tmp   )
-
-      appearance_menu= Menu(parent_menu)
-
-      vars(G)["cu_appearance_"+who]=StringVar(); vars(G)["cu_appearance_"+who].set( vars(G)[who][0])
-      lst =[["White","#ffffff"],["Grey1","#d0d0d0"] ,
-            ["Grey2","#b0b0b0"],["Black","#000000"],
-            "\n",
-            ["Red","#ff7070"], ["Blue","#b4a6ff"],
-     ["Green","#69ff4d"],["DarkGeen","#0a280d"],
-     "\n",
-     ["Yellow","#ffff73"],
-     ["Cyan","#8cffff"],["Purple","#cc55ff"],
-    	  ]
-      for i in lst :
-          if i == "\n" :
-            appearance_menu.add_command(columnbreak=1)
-          else :
-            appearance_menu.add_radiobutton(
-    	    label=i[0],
-    	    command= lambda i=i : BgCl(color=i[1],who=who)  ,
-    	    variable = vars(G)["cu_appearance_"+who], value = i[1]  ,
-    	    )
-
-      appearance_menu.add_command(
-                label="More",
-    	    command=lambda who=who : CrazyColor(who=who) ,
-    	    )
-
-
-      parent_menu.add_cascade(menu=appearance_menu,label=label,underline=0)
-      return
-
-
-    # PANED
-    def PanedConfigMenu(parent_menu,label=""):
-      pconfig_menu= Menu(parent_menu)
-
-      # SAS COLOR
-      def CrazyColor() :
-         import color_interactive
-         tmp = lambda x : PanedConfig( {"bg":x} )
-         color_interactive.Main( func = tmp )
-
-
-      G.cu_p1=StringVar(); G.cu_p1.set(G.paned_dic["sashwidth"])
-      G.cu_p2=StringVar(); G.cu_p2.set(G.paned_dic["sashrelief"])
-      G.cu_p3=StringVar(); G.cu_p3.set(G.paned_dic["bg"])
-
-
-      lst1 =[
-            ["lbl" , "WIDTH"],
-     [str(0),0] , [str(2),2] , [str(4),4] , [str(6),6] ,
-     "\n",
-     ]
-
-
-      lst2= [
-            ["lbl" , "COLOR" ],
-            ["White","#ffffff"],["Grey1","#d0d0d0"] ,
-            ["Grey2","#b0b0b0"],["Black","#000000"],
-            "\n",
-            ["Red","#ff7070"], ["Blue","#b4a6ff"],
-     ["Green","#69ff4d"],["DarkGeen","#0a280d"],
-     "\n",
-     ["Yellow","#ffff73"],
-     ["Cyan","#8cffff"],["Purple","#cc55ff"],
-     ["More", "uselefsss" ],
-    	  ]
-
-      lst3= [
-            ["lbl"    , "relief" ],
-     ["FLAT"   , "flat"   ],
-     ["RAISED" , "raised" ],
-     ["SUNKEN" , "sunken" ],
-     ["GROOVE" , "groove" ],
-     ["RIDGE " , "ridge"  ],
-     "\n",
-     ]
-
-      for j in [  [1,"sashwidth"] , [3,"sashrelief"]  , [2,"bg"] ] :
-        for i in locals()["lst"+str(j[0])] :
-            k = (i,j)
-            if i == "\n" :
-                pconfig_menu.add_command(columnbreak=1)
-            elif i[0] == "lbl" :
-                pconfig_menu.add_command(label=i[1],bg=None,state=DISABLED)
-
-            elif "More" in str(i[0]) :
-              pconfig_menu.add_command( label="More color",command=  CrazyColor )
-            else :
-              pconfig_menu.add_radiobutton(
-      	    label=i[0],
-      	    command= lambda k=k : PanedConfig({k[1][1]:k[0][1]})  ,# k=(i,j)
-      	    variable = vars(G)[ "cu_p" + str(j[0]) ], value = i[1]  ,
-      	    )
-
-
-
-      parent_menu.add_cascade(menu=pconfig_menu,label=label,underline=0)
-      return
-
-
-    def AppearanceMenu1():  # MORE
-      more_menu = Menu(G.abism_menu,**G.submenu_args)
-
-      more_lst = [
-     [ "Color", lambda : AppearanceMenu(more_menu,who="bg",label="Background Color")   ],  # background color
-     [ "Color", lambda : AppearanceMenu(more_menu,who="fg",label="Foreground Color")   ],  # foreground color
-     [ "Color", lambda : PanedConfigMenu(more_menu,label="Config Sash") ],
-     ]
-      for i in more_lst :
-          i[1]()
-      G.abism_menu.menu.add_cascade(menu=more_menu,label="Appearance",underline=0)
-      return
-
-
-
-
-    lst = [
-            [ "About ABISM", MG.AboutAbism ],
-     [ "Quick Guide", lambda : MG.See(pdf="quick_guide.pdf") ],
-     [ "Advanced Manual", lambda : MG.See(pdf="advanced_manual.pdf") ],
-            [ "Appearance", AppearanceMenu1  ],
-     [ "Quit", MG.Quit  ],
-     ]
-    for i in lst :
-        if "Appearance" in i[0] :
-           i[1]()
-        else :
-           G.abism_menu.menu.add_command(label= i[0], command = i[1] )
-
-
-    G.abism_menu['menu'] = G.abism_menu.menu
-    return G.abism_menu
-
-
-
-
-
-    if (type(String) is str or type(String) is unicode) and String != "" :
-      if W.verbose>0 : print "Opening file : " + String
-      W.image_name = String
-      InitImage()
-
-
-    title=W.image_name.split('/')  # we cut the title
-    G.parent.title('Abism (' + title[-1]+')')
-
-
-    #drawing= G.ax1.imshow(W.Im0,vmin=G.min_cut,vmax=G.max_cut)
-    #G.cbar.set_clim(vmin=G.min_cut,vmax=G.max_cut)
-    #G.cbar.draw_all()
-    #G.fig.canvas.draw()
-    return
-
-def ToolMenu():
+def ToolMenu(args):
     G.tool_menu=Menubutton(G.MenuBar,**args)
     G.tool_menu.menu=Menu(G.tool_menu,**G.submenu_args)
 
@@ -220,7 +54,7 @@ def ToolMenu():
     return G.tool_menu
 
 
-def ViewMenu():
+def ViewMenu(args):
     G.scale_menu=Menubutton(G.MenuBar,**args)
     G.scale_menu.menu=Menu(G.scale_menu,**G.submenu_args)
 
@@ -352,7 +186,7 @@ def ViewMenu():
     return G.scale_menu
 
 
-def AnalysisMenu() :
+def AnalysisMenu(args) :
     G.analysis_menu= Menubutton(G.MenuBar,**args)
     G.analysis_menu.menu=Menu(G.analysis_menu,**G.submenu_args)
 
