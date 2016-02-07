f8ab4a1205d04cec2e5e90d86e83386eb2d53faa
tinmarino <tinmarino@gmail.com>
2016-02-07 21:11:29 +0100

    clean ColorGui, the function to color background, now I'm doing foreground

--- Gui/InitGui.py
+++ Gui/InitGui.py
@@ -646,62 +646,6 @@ def ResetLabel(expand=False):
 	  ## APPEARANCES
 	  ###########
 
-def BgCl(color=None,who="bg") :# Background COlor, change the color of ABism take G.bg[0] t
-  ""
-  # Defined colors, set bg, fg, lists
-  if color !=None  : vars(G)[who][0] = color
-  color = vars(G)[who][0]
-
-  # update some dics
-  import GlobalDefiner
-  GlobalDefiner.LinkedColor() # to reload dictionnaries
-
-
-
-  def Frames(): # BG for frames in all_frame
-    for i in G.all_frame :
-      if not "Paned" in i :
-         if W.verbose  > 7 : print i
-	 try :
-            exec(i +"['bg'] = G.bg[0]") in globals(), locals()     # remove 2 first letter because it is G. done for sed , not Paned please
-	 except : pass
-
-  def Canvas() : # BG for figure
-    G.fig.set_facecolor( color )
-    G.fig.canvas.draw()
-    G.figfit.set_facecolor( color )
-    G.figfit.canvas.draw()
-    G.figresult.set_facecolor( color )
-    G.figresult.canvas.draw()
-    for i in G.toolbar.winfo_children() :
-       i[who] = color
-
-  def MenuBut() :
-    for i in G.MenuBar.winfo_children() :
-       i[who] = color
-       BackgroundLoop(i,who=who)
-
-  def Rest() :
-     BackgroundLoop(G.TextPaned,who=who)
-
-  if who == "bg" : Frames()
-  if who =="bg"  : Canvas()
-  MenuBut()
-  Rest() # textframe
-  return
-
-def BackgroundLoop(widget,who="bg"): # read var[0], G.fb Colorize all Label and Frame children,
-  # WIDGETS TO CHANGE, do not change others
-  if who == "bg" :  bolt=    (widget.winfo_class() == "Label")  or (widget.winfo_class()=="Frame") or (widget.winfo_class()=="Menu")  or ( widget.winfo_class() == "Checkbutton")
-  elif who == "fg" :  bolt=    (widget.winfo_class() == "Label")  or (widget.winfo_class()=="Menu")  or ( widget.winfo_class() == "Checkbutton")  or ( widget.winfo_class() == "Button")
-
-  bolt = bolt and ( widget["bg"] !=  G.frame_title_arg["bg"] )  # not change title of frames
-  if bolt :
-    widget[who] = vars(G)[who][0]
-  for i in widget.winfo_children() :
-     BackgroundLoop(i,who=who)
-  return
-
 
 def PanedConfig(arg): # change paned window canvas...
   for i in G.all_frame :
--- Gui/Menu/AbismMenu.py
+++ Gui/Menu/AbismMenu.py
@@ -2,6 +2,8 @@ from Tkinter import *
 import GuyVariables as G
 
 import MyGui as MG
+import ColorGui             # To change the bg and fg, this is in fucntion,
+                            # because obviously it is called more than calling
 
 """
     TODO : make 4, 5 good appearance profiles.
@@ -15,7 +17,7 @@ def AbismMenu(args):                # Menu entry to configure ABSIM
     def AppearanceMenu(parent_menu,who="bg",label="Background") :
       def CrazyColor(who="bg") :
          import color_interactive
-         tmp = ( lambda x, who=who : BgCl(color=x,who=who )   )
+         tmp = ( lambda x, who=who : ColorGui.BgCl(color=x,who=who )   )
          color_interactive.Main( func = tmp   )
 
       appearance_menu= Menu(parent_menu)
@@ -36,7 +38,7 @@ def AbismMenu(args):                # Menu entry to configure ABSIM
           else :
             appearance_menu.add_radiobutton(
                 label=i[0],
-                command=lambda i=i: BgCl(color=i[1], who=who),
+                command=lambda i=i: ColorGui.BgCl(color=i[1], who=who),
                 variable=vars(G)["cu_appearance_"+who], value = i[1]  ,
             )
 
@@ -52,96 +54,95 @@ def AbismMenu(args):                # Menu entry to configure ABSIM
 
     # PANED
     def PanedConfigMenu(parent_menu,label=""):
-      pconfig_menu= Menu(parent_menu)
+        pconfig_menu= Menu(parent_menu)
 
-      # SAS COLOR
-      def CrazyColor() :
-         import color_interactive
-         tmp = lambda x : PanedConfig( {"bg":x} )
-         color_interactive.Main( func = tmp )
-
-
-      G.cu_p1=StringVar(); G.cu_p1.set(G.paned_dic["sashwidth"])
-      G.cu_p2=StringVar(); G.cu_p2.set(G.paned_dic["sashrelief"])
-      G.cu_p3=StringVar(); G.cu_p3.set(G.paned_dic["bg"])
+        # SAS COLOR
+        def CrazyColor() :
+            import color_interactive
+            tmp = lambda x : PanedConfig( {"bg":x} )
+            color_interactive.Main( func = tmp )
 
 
-      lst1 =[
-            ["lbl" , "WIDTH"],
-     [str(0),0] , [str(2),2] , [str(4),4] , [str(6),6] ,
-     "\n",
-     ]
+        G.cu_p1=StringVar(); G.cu_p1.set(G.paned_dic["sashwidth"])
+        G.cu_p2=StringVar(); G.cu_p2.set(G.paned_dic["sashrelief"])
+        G.cu_p3=StringVar(); G.cu_p3.set(G.paned_dic["bg"])
 
 
-      lst2= [
-            ["lbl" , "COLOR" ],
-            ["White","#ffffff"],["Grey1","#d0d0d0"] ,
-            ["Grey2","#b0b0b0"],["Black","#000000"],
-            "\n",
-            ["Red","#ff7070"], ["Blue","#b4a6ff"],
-     ["Green","#69ff4d"],["DarkGeen","#0a280d"],
-     "\n",
-     ["Yellow","#ffff73"],
-     ["Cyan","#8cffff"],["Purple","#cc55ff"],
-     ["More", "uselefsss" ],
-    	  ]
-
-      lst3= [
-            ["lbl"    , "relief" ],
-     ["FLAT"   , "flat"   ],
-     ["RAISED" , "raised" ],
-     ["SUNKEN" , "sunken" ],
-     ["GROOVE" , "groove" ],
-     ["RIDGE " , "ridge"  ],
-     "\n",
-     ]
-
-      for j in [  [1,"sashwidth"] , [3,"sashrelief"]  , [2,"bg"] ] :
-        for i in locals()["lst"+str(j[0])] :
-            k = (i,j)
-            if i == "\n" :
-                pconfig_menu.add_command(columnbreak=1)
-            elif i[0] == "lbl" :
-                pconfig_menu.add_command(label=i[1],bg=None,state=DISABLED)
-
-            elif "More" in str(i[0]) :
-              pconfig_menu.add_command( label="More color",command=  CrazyColor )
-            else :
-              pconfig_menu.add_radiobutton(
-      	    label=i[0],
-      	    command= lambda k=k : PanedConfig({k[1][1]:k[0][1]})  ,# k=(i,j)
-      	    variable = vars(G)[ "cu_p" + str(j[0]) ], value = i[1]  ,
-      	    )
+        lst1 =[
+              ["lbl" , "WIDTH"],
+              [str(0),0] , [str(2),2] , [str(4),4] , [str(6),6] ,"\n",
+        ]
 
 
+        lst2= [
+              ["lbl" , "COLOR" ],
+              ["White","#ffffff"],["Grey1","#d0d0d0"] ,
+              ["Grey2","#b0b0b0"],["Black","#000000"],
+              "\n",
+              ["Red","#ff7070"], ["Blue","#b4a6ff"],
+              ["Green","#69ff4d"],["DarkGeen","#0a280d"],
+              "\n",
+              ["Yellow","#ffff73"],
+              ["Cyan","#8cffff"],["Purple","#cc55ff"],
+              ["More", "uselefsss" ],
+              ]
 
-      parent_menu.add_cascade(menu=pconfig_menu,label=label,underline=0)
-      return
+        lst3= [
+                ["lbl"    , "relief" ],
+                ["FLAT"   , "flat"   ],
+                ["RAISED" , "raised" ],
+                ["SUNKEN" , "sunken" ],
+                ["GROOVE" , "groove" ],
+                ["RIDGE " , "ridge"  ],
+                "\n",
+                ]
+
+        for j in [  [1,"sashwidth"] , [3,"sashrelief"]  , [2,"bg"] ] :
+            for i in locals()["lst"+str(j[0])] :
+                k = (i,j)
+                if i == "\n" :
+                    pconfig_menu.add_command(columnbreak=1)
+                elif i[0] == "lbl" :
+                    pconfig_menu.add_command(label=i[1],bg=None,state=DISABLED)
+
+                elif "More" in str(i[0]) :
+                    pconfig_menu.add_command( label="More color",command=  CrazyColor )
+                else :
+                    pconfig_menu.add_radiobutton(
+                        label=i[0],
+                        command= lambda k=k : PanedConfig({k[1][1]:k[0][1]})  ,# k=(i,j)
+                        variable = vars(G)[ "cu_p" + str(j[0]) ], value = i[1]  ,
+                    )
+
+
+
+        parent_menu.add_cascade(menu=pconfig_menu,label=label,underline=0)
+        return
 
 
     def AppearanceMenu1():  # MORE
-      more_menu = Menu(G.abism_menu,**G.submenu_args)
+        more_menu = Menu(G.abism_menu,**G.submenu_args)
 
-      more_lst = [
-     [ "Color", lambda : AppearanceMenu(more_menu,who="bg",label="Background Color")   ],  # background color
-     [ "Color", lambda : AppearanceMenu(more_menu,who="fg",label="Foreground Color")   ],  # foreground color
-     [ "Color", lambda : PanedConfigMenu(more_menu,label="Config Sash") ],
-     ]
-      for i in more_lst :
-          i[1]()
-      G.abism_menu.menu.add_cascade(menu=more_menu,label="Appearance",underline=0)
-      return
+        more_lst = [
+                [ "Color", lambda : AppearanceMenu(more_menu,who="bg",label="Background Color")   ],  # background color
+                [ "Color", lambda : AppearanceMenu(more_menu,who="fg",label="Foreground Color")   ],  # foreground color
+                [ "Color", lambda : PanedConfigMenu(more_menu,label="Config Sash") ],
+        ]
+        for i in more_lst :
+            i[1]()
+        G.abism_menu.menu.add_cascade(menu=more_menu,label="Appearance",underline=0)
+        return
 
 
 
 
     lst = [
             [ "About ABISM", MG.AboutAbism ],
-     [ "Quick Guide", lambda : MG.See(pdf="quick_guide.pdf") ],
-     [ "Advanced Manual", lambda : MG.See(pdf="advanced_manual.pdf") ],
+            [ "Quick Guide", lambda : MG.See(pdf="quick_guide.pdf") ],
+            [ "Advanced Manual", lambda : MG.See(pdf="advanced_manual.pdf") ],
             [ "Appearance", AppearanceMenu1  ],
-     [ "Quit", MG.Quit  ],
-     ]
+            [ "Quit", MG.Quit  ],
+    ]
     for i in lst :
         if "Appearance" in i[0] :
            i[1]()
@@ -157,17 +158,13 @@ def AbismMenu(args):                # Menu entry to configure ABSIM
 
 
     if (type(String) is str or type(String) is unicode) and String != "" :
-      if W.verbose>0 : print "Opening file : " + String
-      W.image_name = String
-      InitImage()
+        if W.verbose>0 : print "Opening file : " + String
+        W.image_name = String
+        InitImage()
 
 
     title=W.image_name.split('/')  # we cut the title
     G.parent.title('Abism (' + title[-1]+')')
 
 
-    #drawing= G.ax1.imshow(W.Im0,vmin=G.min_cut,vmax=G.max_cut)
-    #G.cbar.set_clim(vmin=G.min_cut,vmax=G.max_cut)
-    #G.cbar.draw_all()
-    #G.fig.canvas.draw()
     return
