e6e839e659b7f7c53a299cc6ef8031215e06e4aa
tinmarino <tinmarino@gmail.com>
2017-01-15 22:18:29 +0100

    Solve imageparameter bug : Put event function in InitGui

--- Gui/InitGui.py
+++ Gui/InitGui.py
@@ -587,6 +587,19 @@ def ImageParameter():
   return
 
 
+
+def GetValueIP(event, destroy=True): # ImageParameter
+    for i in G.image_parameter_list:
+        vars(W.head)[i[1]] = float(vars(G.tkentry)[i[1]].get())
+        # COLOR
+        if vars(W.head)[i[1]] == i[2]:
+            vars(G.tkentry)[i[1]]["bg"] = "#ff9090"
+        else:
+            vars(G.tkentry)[i[1]]["bg"] = "#ffffff"
+        ResetLabel(expand=False)
+
+
+
 def Cube():
       if not W.cube_bool:
         try : G.CubeFrame.destroy()
