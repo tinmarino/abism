986238406ce89a37bcf4f1b39cbe7a0825fc5021
tinmarino <tinmarino@gmail.com>
2020-01-18 17:07:46 -0300

    Comment: abism.py

--- abism.py
+++ abism.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
 """
     Adaptative Background Interactive Strehl Meter,
-            A Software made by
-    Julien Girard and Martin Tourneboeuf
+                    A Software made by
+        Julien Girard and Martin Tourneboeuf
+
+                        Licence
+        Do whatever you want, want wahtever you do
+
+Install
+    1. git clone https://github.com/tinmarino/abism abism_source && cd abism_source
+    2. pip install --user -U git+https://github.com/tinmarino/abism
+
+Run:
+    1. [bash | python] abism.py
+    2. python -m abism
 """
 from abism import __main__
